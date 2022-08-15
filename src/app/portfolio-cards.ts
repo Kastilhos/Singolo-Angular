3e6bdbf78ab393fd1212f26a38b812521533deff
Gabriel Castilhos <g.kastilhos@gmail.com>
2022-08-14 22:47:41 -0300

    Porfolio Session Angular Markup

---
+++ src/app/portfolio-cards.ts
@@ -0,0 +1,76 @@
+import { PortfolioCards } from "./cards";
+
+export const PORT_CARDS: PortfolioCards[] = [
+    {
+        alt: 'Web Design 1',
+        image: 'assets/portfolio-1.png',
+        tag: "web-design",
+        order: 1
+    },
+    {
+        alt: 'Artwork 1',
+        image: 'assets/portfolio-2.png',
+        tag: "artwork",
+        order: 2
+    },
+    {
+        alt: 'Web Design 2',
+        image: 'assets/portfolio-3.png',
+        tag: "web-design",
+        order: 3
+    },
+    {
+        alt: 'Graphic Design 1',
+        image: 'assets/portfolio-4.png',
+        tag: "graph-design",
+        order: 4
+    },
+    {
+        alt: 'Artwork 2',
+        image: 'assets/portfolio-5.png',
+        tag: "artwork",
+        order: 5
+    },
+    {
+        alt: 'Graph Design 2',
+        image: 'assets/portfolio-6.png',
+        tag: "graph-design",
+        order: 6
+    },
+    {
+        alt: 'Artwork 3',
+        image: 'assets/portfolio-7.png',
+        tag: "artwork",
+        order: 7
+    },
+    {
+        alt: 'Graphic Design 3',
+        image: 'assets/portfolio-8.png',
+        tag: "graph-design",
+        order: 8
+    },
+    {
+        alt: 'Artwork 4',
+        image: 'assets/portfolio-9.png',
+        tag: "artwork",
+        order: 9
+    },
+    {
+        alt: 'Web Design 3',
+        image: 'assets/portfolio-10.png',
+        tag: "web-design",
+        order: 10
+    },
+    {
+        alt: 'Graphic Design 4',
+        image: 'assets/portfolio-11.png',
+        tag: "graph-design",
+        order: 11
+    },
+    {
+        alt: 'Artwork 5',
+        image: 'assets/portfolio-12.png',
+        tag: "artwork",
+        order: 12
+    }
+];
